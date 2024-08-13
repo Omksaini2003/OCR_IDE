85015e1d224f0d4ba2fa57dbd5f5c96cfc5f7f9f
kunal90803 <palk27530@gmail.com>
2024-08-13 18:29:55 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Box} from "@chakra-ui/react"
+import CodeEditor from "./components/CodeEditor"
+function App() {
+  
+
+  return (
+    <Box minH="100vh" px="10px" bg="#0f0a19" py="8px">
+            <CodeEditor/>
+    </Box>
+  )
+}
+
+export default App
